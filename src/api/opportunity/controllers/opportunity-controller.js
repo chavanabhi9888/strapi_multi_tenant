32ff0565b5de4dbebfa5ff0cb5160fc855b2b5aa
Atharva0223 <atharva@geexu.in>
2023-04-18 17:58:42 +0530

    added endpoints for pre fetch and dashboard

--- src/api/opportunity/controllers/opportunity-controller.js
+++ src/api/opportunity/controllers/opportunity-controller.js
@@ -1,6 +1,7 @@
 const { connect } = require("../../../../config/pg");
 
 module.exports = {
+  //dashboard
   //This will fetch all the new opportunities on dashboard
   async find(ctx) {
     try {
@@ -13,6 +14,8 @@ module.exports = {
       COALESCE(opp.city, '') AS "City",
       COALESCE(opp_image.url, '') AS "Opportunity image",
       COALESCE(opp.months) AS "Duration",
+      COALESCE(opp.start_on) AS "Start Date",
+      COALESCE(opp.end_on) AS "End Date",
       COALESCE(ROUND(AVG(r.value), 1), 0) AS "Rating",
 	  opp.published_at
     FROM 
@@ -31,6 +34,8 @@ module.exports = {
     GROUP BY 
       org_logo.url,
       opp.months,
+      opp.start_on,
+      opp.end_on,
       org.name,
       opp.profile,
       opp.city,
@@ -49,8 +54,8 @@ module.exports = {
     }
   },
 
-  //fetching responsibilities and skills of an individual opportunities
-  async findPost(ctx) {
+  //fetching responsibilities and skills of an individual opportunity
+  async findOpportunity(ctx) {
     try {
       const client = await connect();
       const query = `
@@ -78,7 +83,9 @@ module.exports = {
         opp.profile,
         opp_image.url,
         opp.responsibilities,
-        opp.skills;
+        opp.skills
+    ORDER BY
+	      opp.published_at DESC;
     `;
 
       const data = await client.query(query, [ctx.params.id]);
@@ -91,36 +98,39 @@ module.exports = {
     }
   },
 
-  //displaying facilities, terms and support
-  async findDetails(ctx) {
+  //displaying facilities, terms and support of an individual opportunity
+  async findOpportunityDetails(ctx) {
     try {
       const client = await connect();
       const query = `
       SELECT
-	      opp_image.url AS "Opportunity image",
-	      org.name AS "Organization name",
-        opp.profile AS "Opportunity profile",
-        opp.facilities AS "Facilities provided",
-        opp.support AS "Support provided",
-        opp.terms AS "Terms and Conditions"
-      FROM 
-        opportunities opp
-      JOIN opportunities_organization_links ool ON opp.id = ool.opportunity_id
-      JOIN organizations org ON ool.organization_id = org.id
-      JOIN opportunities_organization_user_links ooul ON opp.id = ooul.opportunity_id
-      JOIN files_related_morphs frm_logo ON frm_logo.related_id = opp.id AND frm_logo.field = 'logo'
-      JOIN files org_logo ON frm_logo.file_id = org_logo.id
-      JOIN files_related_morphs frm_image ON frm_image.related_id = opp.id AND frm_image.field = 'image'
-      JOIN files opp_image ON frm_image.file_id = opp_image.id
-      WHERE
-        opp.id = $1 AND is_deleted = false
-      GROUP BY 
-        opp_image.url,
-        org.name,
-        opp.profile,
-        opp.facilities,
-        opp.support,
-	      opp.terms`;
+	opp_image.url AS "Opportunity image",
+	org.name AS "Organization name",
+    opp.profile AS "Opportunity profile",
+    opp.facilities AS "Facilities provided",
+    opp.support AS "Support provided",
+    opp.terms AS "Terms and Conditions"
+FROM 
+    opportunities opp
+LEFT JOIN opportunities_organization_links ool ON opp.id = ool.opportunity_id
+LEFT JOIN organizations org ON ool.organization_id = org.id
+LEFT JOIN opportunities_organization_user_links ooul ON opp.id = ooul.opportunity_id
+LEFT JOIN files_related_morphs frm_logo ON frm_logo.related_id = ool.organization_id AND frm_logo.field = 'logo'
+LEFT JOIN files org_logo ON frm_logo.file_id = org_logo.id
+LEFT JOIN files_related_morphs frm_image ON frm_image.related_id = opp.id AND frm_image.field = 'image'
+LEFT JOIN files opp_image ON frm_image.file_id = opp_image.id
+WHERE
+    opp.id = 1 AND is_deleted = false
+GROUP BY 
+    opp_image.url,
+    org.name,
+    opp.profile,
+    opp.facilities,
+    opp.support,
+	opp.terms
+  ORDER BY
+	  opp.published_at DESC;
+        `;
 
       const data = await client.query(query, [ctx.params.id]);
 
@@ -132,17 +142,52 @@ module.exports = {
     }
   },
 
-  //deleting an opportunity
-  async delete(ctx) {
+  //View all ongoing and waiting opportunities
+  async viewOngoing(ctx) {
     try {
       const client = await connect();
       const query = `
-      UPDATE
-        opportunities
-      SET
-        is_deleted = true
-      Where
-        id = $1`;
+        SELECT
+        org_logo.url AS "Organization logo",
+        COALESCE(org.name, '') AS "Organization name",
+      os.status AS "Opportunity Status",
+        COALESCE(opp.profile, '') AS "Opportunity profile",
+        COALESCE(opp.city, '') AS "City",
+        COALESCE(opp_image.url, '') AS "Opportunity image",
+        COALESCE(opp.months) AS "Duration",
+        COALESCE(opp.start_on) AS "Start Date",
+        COALESCE(opp.end_on) AS "End Date",
+        COALESCE(ROUND(AVG(r.value), 1), 0) AS "Rating"
+      FROM 
+        opportunities opp
+        LEFT JOIN opportunities_organization_links ool ON opp.id = ool.opportunity_id
+        LEFT JOIN organizations org ON ool.organization_id = org.id
+        LEFT JOIN opportunities_organization_user_links ooul ON opp.id = ooul.opportunity_id
+        LEFT JOIN ratings_opportunity_links rol ON rol.opportunity_id = opp.id
+        LEFT JOIN ratings r ON r.id = rol.rating_id
+        LEFT JOIN files_related_morphs frm_logo ON frm_logo.related_id = ool.organization_id AND frm_logo.field = 'logo'
+        LEFT JOIN files org_logo ON frm_logo.file_id = org_logo.id
+        LEFT JOIN files_related_morphs frm_image ON frm_image.related_id = opp.id AND frm_image.field = 'image'
+        LEFT JOIN files opp_image ON frm_image.file_id = opp_image.id
+      LEFT JOIN opportunity_statuses_opportunity_links osol ON opp.id = osol.opportunity_id
+      LEFT JOIN opportunity_statuses os ON os.id = osol.opportunity_status_id
+      LEFT JOIN opportunity_statuses_user_links osul ON os.id = osul.opportunity_status_id
+      LEFT JOIN up_users uu ON uu.id = osul.user_id
+  WHERE
+      uu.id = 1 AND os.status = 'ongoing' OR os.status = 'waiting'
+      GROUP BY
+        org_logo.url,
+        opp.months,
+      os.status,
+        opp.start_on,
+        opp.end_on,
+        org.name,
+        opp.profile,
+        opp.city,
+        opp_image.url
+    ORDER BY
+          opp.published_at DESC;
+      `;
 
       const data = await client.query(query, [ctx.params.id]);
 
@@ -154,43 +199,109 @@ module.exports = {
     }
   },
 
-  //Find Ongoing tasks
-  async findOngoing(ctx) {
+  //View all completed opportunities
+  async viewCompleted(ctx) {
+    try {
+      const client = await connect();
+      const query = `
+        SELECT
+        org_logo.url AS "Organization logo",
+        COALESCE(org.name, '') AS "Organization name",
+      os.status AS "Opportunity Status",
+        COALESCE(opp.profile, '') AS "Opportunity profile",
+        COALESCE(opp.city, '') AS "City",
+        COALESCE(opp_image.url, '') AS "Opportunity image",
+        COALESCE(opp.months) AS "Duration",
+        COALESCE(opp.start_on) AS "Start Date",
+        COALESCE(opp.end_on) AS "End Date",
+        COALESCE(ROUND(AVG(r.value), 1), 0) AS "Rating"
+      FROM 
+        opportunities opp
+        LEFT JOIN opportunities_organization_links ool ON opp.id = ool.opportunity_id
+        LEFT JOIN organizations org ON ool.organization_id = org.id
+        LEFT JOIN opportunities_organization_user_links ooul ON opp.id = ooul.opportunity_id
+        LEFT JOIN ratings_opportunity_links rol ON rol.opportunity_id = opp.id
+        LEFT JOIN ratings r ON r.id = rol.rating_id
+        LEFT JOIN files_related_morphs frm_logo ON frm_logo.related_id = ool.organization_id AND frm_logo.field = 'logo'
+        LEFT JOIN files org_logo ON frm_logo.file_id = org_logo.id
+        LEFT JOIN files_related_morphs frm_image ON frm_image.related_id = opp.id AND frm_image.field = 'image'
+        LEFT JOIN files opp_image ON frm_image.file_id = opp_image.id
+      LEFT JOIN opportunity_statuses_opportunity_links osol ON opp.id = osol.opportunity_id
+      LEFT JOIN opportunity_statuses os ON os.id = osol.opportunity_status_id
+      LEFT JOIN opportunity_statuses_user_links osul ON os.id = osul.opportunity_status_id
+      LEFT JOIN up_users uu ON uu.id = osul.user_id
+  WHERE
+      uu.id = 1 AND os.status = 'completed'
+      GROUP BY
+        org_logo.url,
+        opp.months,
+      os.status,
+        opp.start_on,
+        opp.end_on,
+        org.name,
+        opp.profile,
+        opp.city,
+        opp_image.url
+    ORDER BY
+          opp.published_at DESC;
+      `;
+
+      const data = await client.query(query, [ctx.params.id]);
+
+      ctx.send({
+        data: data.rows,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  //splashscreen
+  //This will fetch all the new opportunities on splash screen (top 5)
+  async findTopFive(ctx) {
     try {
       const client = await connect();
       const query = `
       SELECT 
       org_logo.url AS "Organization logo",
-      org.name AS "Organization name",
-      opp.profile AS "Opportunity profile",
-      opp.city AS "City",
-      opp_image.url AS "Opportunity image",
-      opp.months AS "Duration",
-      ROUND(AVG(r.value), 1) AS "Rating"
+      COALESCE(org.name, '') AS "Organization name",
+      COALESCE(opp.profile, '') AS "Opportunity profile",
+      COALESCE(opp.city, '') AS "City",
+      COALESCE(opp_image.url, '') AS "Opportunity image",
+      COALESCE(opp.months) AS "Duration",
+      COALESCE(opp.start_on) AS "Start Date",
+      COALESCE(opp.end_on) AS "End Date",
+      COALESCE(ROUND(AVG(r.value), 1), 0) AS "Rating"
     FROM 
       opportunities opp
-      JOIN opportunities_organization_links ool ON opp.id = ool.opportunity_id
-      JOIN organizations org ON ool.organization_id = org.id
-      JOIN opportunities_organization_user_links ooul ON opp.id = ooul.opportunity_id
-      JOIN ratings_opportunity_links rol ON rol.opportunity_id = opp.id
-      JOIN ratings r ON r.id = rol.rating_id
-      JOIN files_related_morphs frm_logo ON frm_logo.related_id = opp.id AND frm_logo.field = 'logo'
-      JOIN files org_logo ON frm_logo.file_id = org_logo.id
-      JOIN files_related_morphs frm_image ON frm_image.related_id = opp.id AND frm_image.field = 'image'
-      JOIN files opp_image ON frm_image.file_id = opp_image.id
+      LEFT JOIN opportunities_organization_links ool ON opp.id = ool.opportunity_id
+      LEFT JOIN organizations org ON ool.organization_id = org.id
+      LEFT JOIN opportunities_organization_user_links ooul ON opp.id = ooul.opportunity_id
+      LEFT JOIN ratings_opportunity_links rol ON rol.opportunity_id = opp.id
+      LEFT JOIN ratings r ON r.id = rol.rating_id
+      LEFT JOIN files_related_morphs frm_logo ON frm_logo.related_id = ool.organization_id AND frm_logo.field = 'logo'
+      LEFT JOIN files org_logo ON frm_logo.file_id = org_logo.id
+      LEFT JOIN files_related_morphs frm_image ON frm_image.related_id = opp.id AND frm_image.field = 'image'
+      LEFT JOIN files opp_image ON frm_image.file_id = opp_image.id
     WHERE
       opp.is_deleted = false
     GROUP BY 
       org_logo.url,
       opp.months,
+      opp.start_on,
+      opp.end_on,
       org.name,
       opp.profile,
       opp.city,
-      opp_image.url;
+      opp_image.url,
+	  opp.published_at
+	ORDER BY
+	  opp.published_at DESC
+	LIMIT
+	  5
     `;
 
-      const data = await client.query(query, [ctx.params.id]);
-
+      const data = await client.query(query);
       ctx.send({
         data: data.rows,
       });
@@ -198,4 +309,160 @@ module.exports = {
       console.log(error);
     }
   },
+
+  //This will fetch all the ongoing and waiting opportunities on splash screen (top 5)
+  async findTopFiveOngoing(ctx) {
+    try {
+      const client = await connect();
+      const query = `
+      SELECT
+      org_logo.url AS "Organization logo",
+      COALESCE(org.name, '') AS "Organization name",
+    os.status AS "Opportunity Status",
+      COALESCE(opp.profile, '') AS "Opportunity profile",
+      COALESCE(opp.city, '') AS "City",
+      COALESCE(opp_image.url, '') AS "Opportunity image",
+      COALESCE(opp.months) AS "Duration",
+      COALESCE(opp.start_on) AS "Start Date",
+      COALESCE(opp.end_on) AS "End Date",
+      COALESCE(ROUND(AVG(r.value), 1), 0) AS "Rating"
+    FROM 
+      opportunities opp
+      LEFT JOIN opportunities_organization_links ool ON opp.id = ool.opportunity_id
+      LEFT JOIN organizations org ON ool.organization_id = org.id
+      LEFT JOIN opportunities_organization_user_links ooul ON opp.id = ooul.opportunity_id
+      LEFT JOIN ratings_opportunity_links rol ON rol.opportunity_id = opp.id
+      LEFT JOIN ratings r ON r.id = rol.rating_id
+      LEFT JOIN files_related_morphs frm_logo ON frm_logo.related_id = ool.organization_id AND frm_logo.field = 'logo'
+      LEFT JOIN files org_logo ON frm_logo.file_id = org_logo.id
+      LEFT JOIN files_related_morphs frm_image ON frm_image.related_id = opp.id AND frm_image.field = 'image'
+      LEFT JOIN files opp_image ON frm_image.file_id = opp_image.id
+    LEFT JOIN opportunity_statuses_opportunity_links osol ON opp.id = osol.opportunity_id
+    LEFT JOIN opportunity_statuses os ON os.id = osol.opportunity_status_id
+    LEFT JOIN opportunity_statuses_user_links osul ON os.id = osul.opportunity_status_id
+    LEFT JOIN up_users uu ON uu.id = osul.user_id
+WHERE
+    uu.id = 1 AND os.status = 'ongoing' OR os.status = 'waiting'
+    GROUP BY
+      org_logo.url,
+      opp.months,
+    os.status,
+      opp.start_on,
+      opp.end_on,
+      org.name,
+      opp.profile,
+      opp.city,
+      opp_image.url
+  ORDER BY
+        opp.published_at DESC
+	LIMIT
+	  5
+    `;
+
+      const data = await client.query(query);
+      ctx.send({
+        data: data.rows,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  //This will fetch all the completed opportunities on splash screen (top 5)
+  async findTopFiveCompleted(ctx) {
+    try {
+      const client = await connect();
+      const query = `
+      SELECT
+        org_logo.url AS "Organization logo",
+        COALESCE(org.name, '') AS "Organization name",
+      os.status AS "Opportunity Status",
+        COALESCE(opp.profile, '') AS "Opportunity profile",
+        COALESCE(opp.city, '') AS "City",
+        COALESCE(opp_image.url, '') AS "Opportunity image",
+        COALESCE(opp.months) AS "Duration",
+        COALESCE(opp.start_on) AS "Start Date",
+        COALESCE(opp.end_on) AS "End Date",
+        COALESCE(ROUND(AVG(r.value), 1), 0) AS "Rating"
+      FROM 
+        opportunities opp
+        LEFT JOIN opportunities_organization_links ool ON opp.id = ool.opportunity_id
+        LEFT JOIN organizations org ON ool.organization_id = org.id
+        LEFT JOIN opportunities_organization_user_links ooul ON opp.id = ooul.opportunity_id
+        LEFT JOIN ratings_opportunity_links rol ON rol.opportunity_id = opp.id
+        LEFT JOIN ratings r ON r.id = rol.rating_id
+        LEFT JOIN files_related_morphs frm_logo ON frm_logo.related_id = ool.organization_id AND frm_logo.field = 'logo'
+        LEFT JOIN files org_logo ON frm_logo.file_id = org_logo.id
+        LEFT JOIN files_related_morphs frm_image ON frm_image.related_id = opp.id AND frm_image.field = 'image'
+        LEFT JOIN files opp_image ON frm_image.file_id = opp_image.id
+      LEFT JOIN opportunity_statuses_opportunity_links osol ON opp.id = osol.opportunity_id
+      LEFT JOIN opportunity_statuses os ON os.id = osol.opportunity_status_id
+      LEFT JOIN opportunity_statuses_user_links osul ON os.id = osul.opportunity_status_id
+      LEFT JOIN up_users uu ON uu.id = osul.user_id
+  WHERE
+      uu.id = 1 AND os.status = 'completed'
+      GROUP BY
+        org_logo.url,
+        opp.months,
+      os.status,
+        opp.start_on,
+        opp.end_on,
+        org.name,
+        opp.profile,
+        opp.city,
+        opp_image.url
+    ORDER BY
+          opp.published_at DESC
+	LIMIT
+	  5
+    `;
+
+      const data = await client.query(query);
+      ctx.send({
+        data: data.rows,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  
+  //Here the user will be able to apply to opportunities
+  // async applyOpportunity(ctx) {
+  //   try {
+  //     const client = await connect();
+  //     const query = `
+      
+  //   `;
+
+  //     const data = await client.query(query, [ctx.params.id]);
+
+  //     ctx.send({
+  //       data: data.rows,
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // },
+
+  //deleting an opportunity
+  // async delete(ctx) {
+  //   try {
+  //     const client = await connect();
+  //     const query = `
+  //     UPDATE
+  //       opportunities
+  //     SET
+  //       is_deleted = true
+  //     Where
+  //       id = $1`;
+
+  //     const data = await client.query(query, [ctx.params.id]);
+
+  //     ctx.send({
+  //       data: data.rows,
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // },
 };
--- src/api/opportunity/routes/opportunity-route.js
+++ src/api/opportunity/routes/opportunity-route.js
@@ -1,5 +1,7 @@
 module.exports = {
     routes: [
+      //dashboard fetch
+      //find
       {
         method: "GET",
         path: "/find",
@@ -9,41 +11,98 @@ module.exports = {
           middleware: [],
         },
       },
+      //find Opportunity
       {
         method: "GET",
-        path: "/findPost/:id",
-        handler: "opportunity-controller.findPost",
+        path: "/findOpportunity/:id",
+        handler: "opportunity-controller.findOpportunity",
         config: {
           policies: [],
           middleware: [],
         },
       },
+      //find Opportunity Details
       {
         method: "GET",
-        path: "/findDetails/:id",
-        handler: "opportunity-controller.findDetails",
+        path: "/findOpportunityDetails/:id",
+        handler: "opportunity-controller.findOpportunityDetails",
         config: {
           policies: [],
           middleware: [],
         },
       },
+      //view Ongoing
       {
-        method: "DELETE",
-        path: "/delete/:id",
-        handler: "opportunity-controller.delete",
+        method: "GET",
+        path: "/viewOngoing",
+        handler: "opportunity-controller.viewOngoing",
         config: {
           policies: [],
           middleware: [],
         },
       },
+      //view Completed
       {
         method: "GET",
-        path: "/findOngoing",
-        handler: "opportunity-controller.findOngoing",
+        path: "/viewCompleted",
+        handler: "opportunity-controller.viewCompleted",
         config: {
           policies: [],
           middleware: [],
         },
-      }
+      },
+//--------------------------------------------------------------------------------------
+      //splash screen per fetch data
+      //find top 5 new opportunities
+      {
+        method: "GET",
+        path: "/findTopFive",
+        handler: "opportunity-controller.findTopFive",
+        config: {
+          policies: [],
+          middleware: [],
+        },
+      },
+      //find top 5 ongoing and waiting opportunities
+      {
+        method: "GET",
+        path: "/findTopFiveOngoing",
+        handler: "opportunity-controller.findTopFiveOngoing",
+        config: {
+          policies: [],
+          middleware: [],
+        },
+      },
+      //find top 5 completed opportunities
+      {
+        method: "GET",
+        path: "/findTopFiveCompleted",
+        handler: "opportunity-controller.findTopFiveCompleted",
+        config: {
+          policies: [],
+          middleware: [],
+        },
+      },
+//--------------------------------------------------------------------------------------
+      //apply Opportunity
+      // {
+      //   method: "GET",
+      //   path: "/applyOpportunity",
+      //   handler: "opportunity-controller.applyOpportunity",
+      //   config: {
+      //     policies: [],
+      //     middleware: [],
+      //   },
+      // },
+      //delete An Opportunity
+      // {
+      //   method: "DELETE",
+      //   path: "/delete/:id",
+      //   handler: "opportunity-controller.delete",
+      //   config: {
+      //     policies: [],
+      //     middleware: [],
+      //   },
+      // },
     ],
   };
